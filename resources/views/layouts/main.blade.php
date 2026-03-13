0d4b4bd36d8df0ba83bc1f60dfdd6d84168587ab
Lucas Gabriel Risso <lucas.risso@prefeitura.araras>
2026-03-13 12:30:04 -0300

    Primeiro commit do projeto Laravel

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title> @yield('title')</title>
+        <!-- fontes googel -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        
+        <!-- css bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <!-- css da aplicação -->
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+    <body >
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="collapse navbar-collapse" id="navbar">
+        <a href="/" class="navbar-brand">
+        <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+        </a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="/" class="nav-link">Eventos</a>
+            </li>
+                        <li class="nav-item">
+                <a href="/events/create" class="nav-link">Criar Eventos</a>
+            </li>
+                        <li class="nav-item">
+                <a href="/" class="nav-link">Entrar</a>
+            </li>
+                            <li class="nav-item">
+                <a href="/" class="nav-link">Cadastrar</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+    </header>
+    @yield('content')
+
+    <footer>
+    <p>HDC Events &copy;2026</p>
+    </footer>
+    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+
+
+
+Route::get('/',[EventController::class,'index']);
+Route::get('/events/create',[EventController::class,'create']);
+route::post('/events',[EventController::class,'store']);
+
+route::get('/contact',function(){
+    return view('contact');
+});
